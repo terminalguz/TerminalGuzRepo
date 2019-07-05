84b19f2b599b1595a277a320e18f658b9db3834f
Boopathighub471 <52530216+Boopathighub471@users.noreply.github.com>
2019-07-05 16:39:58 +0530

    DummyFile
    dummy file check

---
+++ Helper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DashboardIntel
+{
+    class Helper
+    {
+        public string RallyUsername { get; set; }
+        public string RallyPassword { get; set; }
+        public string RallyApiKeyToken { get; set; }
+        public string ServiceNowClientSectrete { get; set; }
+        public string ServiceNowClientId { get; set; }
+        public string ServiceNowAccessToken { get; set; }
+    }
+}
